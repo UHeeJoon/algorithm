e0b54980735a20514317ca2dce3a9b734e7e6492
YooHeeJoon <92840513+UHeeJoon@users.noreply.github.com>
2024-04-05 00:26:55 +0900

    [Bronze IV] Title: 빠른 A+B, Time: 792 ms, Memory: 6796 KB -BaekjoonHub

---
+++ 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	reader := bufio.NewReader(os.Stdin)
+	writer := bufio.NewWriter(os.Stdout)
+	defer writer.Flush()
+	var a, b, n int
+	fmt.Fscan(reader, &n)
+	for i := 0; i < n; i++ {
+		fmt.Fscan(reader, &a, &b)
+		fmt.Fprintln(writer, a+b)
+	}
+
+}
