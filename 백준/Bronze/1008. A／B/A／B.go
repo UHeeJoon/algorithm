c4f8031ef9282ad44ff47de2ce6f570148f99e3f
YooHeeJoon <92840513+UHeeJoon@users.noreply.github.com>
2024-04-04 20:42:21 +0900

    [Bronze V] Title: A/B, Time: 4 ms, Memory: 872 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	var reader *bufio.Reader = bufio.NewReader(os.Stdin)
+	var writer *bufio.Writer = bufio.NewWriter(os.Stdout)
+	defer writer.Flush()
+	var a, b float64
+	fmt.Fscan(reader, &a, &b)
+	fmt.Fprintln(writer, a/b)
+}
