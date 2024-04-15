144ff919b44606cca9e2d7d4d88baa6a26f1f6a0
YooHeeJoon <92840513+UHeeJoon@users.noreply.github.com>
2024-04-15 18:45:30 +0900

    [Bronze III] Title: 오븐 시계, Time: 4 ms, Memory: 876 KB -BaekjoonHub

---
+++ 백준/Bronze/2525. 오븐 시계/오븐 시계.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	reader := bufio.NewReader(os.Stdin)
+	writer := bufio.NewWriter(os.Stdout)
+	defer writer.Flush()
+	var a, b, c int
+	fmt.Fscan(reader, &a, &b, &c)
+	fmt.Fprintln(writer, (a+(b+c)/60)%24, (b+c)%60)
+}
