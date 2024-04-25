dd0fa7c1e16f8b352b5775b9978f08532b9b1aca
YooHeeJoon <92840513+UHeeJoon@users.noreply.github.com>
2024-04-25 18:14:10 +0900

    [Bronze II] Title: 소수 찾기, Time: 8 ms, Memory: 888 KB -BaekjoonHub

---
+++ 백준/Bronze/1978. 소수 찾기/소수 찾기.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"math"
+	"os"
+	"strconv"
+	"strings"
+)
+
+var (
+	reader = bufio.NewReader(os.Stdin)
+	writer = bufio.NewWriter(os.Stdout)
+)
+
+func main() {
+	defer writer.Flush()
+	var (
+		primes = make([]bool, 1010)
+		n      = 0
+		count  = 0
+	)
+	primes[1] = true
+	for i := 2; i <= int(math.Sqrt(1000)); i++ {
+		if primes[i] {
+			continue
+		}
+		for j := i + i; j <= 1000; j += i {
+			primes[j] = true
+		}
+	}
+	fmt.Fscanln(reader, &n)
+	input, _ := reader.ReadString('\n')
+	nums := strings.Fields(input)
+	for _, num := range nums {
+		data, _ := strconv.Atoi(num)
+		if !primes[data] {
+			count++
+		}
+	}
+	fmt.Fprintln(writer, count)
+}
