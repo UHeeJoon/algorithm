56b0843a0d31762d00c2c010a1556729faf2e825
YooHeeJoon <92840513+UHeeJoon@users.noreply.github.com>
2024-10-07 14:01:30 +0900

    [Bronze III] Title: 별 찍기 - 9, Time: 0 ms, Memory: 13208 KB -BaekjoonHub

--- 백준/Bronze/2446. 별 찍기 － 9/별 찍기 － 9.rs
+++ 백준/Bronze/2446. 별 찍기 － 9/별 찍기 － 9.rs
@@ -1,11 +1,14 @@
+use std::fmt::Write;
 use std::io::{self, BufRead, BufReader};
 
 fn main() {
     let mut reader = BufReader::new(io::stdin().lock());
+    let mut output: String = String::new();
     let mut input: String = String::new();
     reader.read_line(&mut input).unwrap();
     let input = input.trim().parse().unwrap();
     for i in (1..=input).rev().chain(2..=input) {
-        println!("{}{}", " ".repeat(input - i), "*".repeat(2 * i - 1));
+        writeln!(output, "{}{}", " ".repeat(input - i), "*".repeat(2 * i - 1)).ok();
     }
+    println!("{output}");
 }
